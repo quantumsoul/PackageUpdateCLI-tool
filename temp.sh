f684ce13e9d417be8178fba3bb160c7c2bb8f555
aviwer <prakhar24012000@gmail.com>
2022-06-02 06:34:19 +0530

    package-update

--- temp.sh
+++ temp.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/bash
 pwd
 gh repo fork "name1" --clone=true
+pause
 cd "name2"
 json -I -f package.json -e "this.name3=0.23.0"
 git add .
 git commit -m "package-update"
 gh pr create --repo $name1 --title "package-update from prakhar" --body "$name3 package updated"
-pause
+
 
