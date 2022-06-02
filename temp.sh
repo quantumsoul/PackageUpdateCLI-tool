95514d143dd30b497b0ab9e22a7f6051863d7cc5
aviwer <prakhar24012000@gmail.com>
2022-06-02 06:32:15 +0530

    package-update

--- temp.sh
+++ temp.sh
@@ -6,5 +6,5 @@ json -I -f package.json -e "this.name3=0.23.0"
 git add .
 git commit -m "package-update"
 gh pr create --repo $name1 --title "package-update from prakhar" --body "$name3 package updated"
-
+pause
 
