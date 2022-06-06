6bcca790f07cc79231bfa7202417fb045d88aeb5
aviwer <prakhar24012000@gmail.com>
2022-06-06 13:53:37 +0530

    package-update

--- index.js
+++ index.js
@@ -128,6 +128,17 @@ else if(process.argv[2] == "-update"){
                     }
                     console.log(`stdout: ${stdout}`);
                 });
+                execSync(`git push origin main`, (error, stdout, stderr) => {
+                    if (error) {
+                        console.log(`error: ${error.message}`);
+                        return;
+                    }
+                    if (stderr) {
+                        console.log(`stderr: ${stderr}`);
+                        return;
+                    }
+                    console.log(`stdout: ${stdout}`);
+                });                
                 execSync(`gh pr create --repo ${url} --title "package-update from prakhar" --body " relevent package updated"`, (error, stdout, stderr) => {
                     if (error) {
                         console.log(`error: ${error.message}`);
