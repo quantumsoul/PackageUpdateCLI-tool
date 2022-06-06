9cdc1db3a8d8ac1f9de8d734d5c31e1e5fc9e53e
prakhar nigam <39532087+quantumsoul@users.noreply.github.com>
2022-06-06 17:38:19 +0530

    Delete temp.sh

--- temp.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-pwd
-gh repo fork name1 --clone=true
-cd name2
-json -I -f package.json -e " this.name3 = 0.23.0"
-git add .
-git commit -m "package-update"
-gh pr create --repo name1 --title "package-update from prakhar" --body " relevent package updated"
-pause
